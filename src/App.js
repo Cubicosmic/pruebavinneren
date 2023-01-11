93d3d82bf55389b294373652b579359412e74e2a
Gael García <cubicosmic@gmail.com>
2023-01-11 15:06:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Foquito from './Foquito';
 
 function App() {
+
+  const [color, setColor] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if(color<2){setColor(color+1)}else{setColor(0)}
+    },2000)
+  },[color])
+
   return (
     <div style={{width: "100%", height:"100vh", background: "linear-gradient(#DD3399, #33DD99)"}} className='flex'>
       <div style={{width: "50%", height: "150px", borderRadius: "50px", background: "#DDDDDD"}} className='flex-a'>
-        <Foquito bg={'#FF0000'} dy={2} />
-        <Foquito bg={'#FFFF00'} dy={4} />
-        <Foquito bg={'#00FF00'} dy={6} />
+        <div className='foquito' style={color === 0 ? {background: 'red'}:{background:'gray'}}></div>
+        <div className='foquito' style={color === 1 ? {background: 'yellow'}:{background:'gray'}}></div>
+        <div className='foquito' style={color === 2 ? {background: 'green'}:{background:'gray'}}></div>
       </div>
     </div>
   );
